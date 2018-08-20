a110d63fe9f871fdf6116df7dc25b55f2ee7a249
wangzheng1999 <33979901+wangzheng1999@users.noreply.github.com>
2018-08-20 17:23:26 +0800

    Update Example1.java

--- Frame窗体/src/Example1.java
+++ Frame窗体/src/Example1.java
@@ -12,15 +12,15 @@ public class Example1 extends JFrame{
 	public void CreateJFrame(String title) {
 		JFrame jf=new JFrame(title);
 		Container container=jf.getContentPane();
-		JLabel jl=new JLabel("这是一个JFrame窗体");
+		JLabel jl=new JLabel("杩欐槸涓�涓狫Frame绐椾綋");
 		jl.setHorizontalAlignment(SwingConstants.CENTER);
 		container.add(jl);
 		container.setBackground(Color.white);
 		jf.setVisible(true);
 		jf.setSize(200,150);
-		//jf。setDefaultCloseOperation(WindowConstants。EXIT_ON_CLOSE);
+		//jf銆俿etDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 	}
 	public static void main(String args[]) {
-		new Example1().CreateJFrame("创建一个JFrame窗体");
+		new Example1().CreateJFrame("鍒涘缓涓�涓狫Frame绐椾綋");
 	}
 }
